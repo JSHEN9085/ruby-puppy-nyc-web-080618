9b95698639abcd9ee6d86cd6f0dd3497529c1449
github username <github email address>
2018-08-08 21:35:18 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,39 @@
+# class Dog
+#   attr_accessor :name
+#
+#   @@all = []
+#
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+#
+#   def self.clear_all
+#     @@all.clear
+#   end
+#
+#   def self.all
+#     dog = name.split("\n")
+#     @@all.each do |dog|
+#       puts dog.name
+#     end
+#   end
+#
+# end ## end of the class
+
 class Dog
-  attr_accessor :name
-
-  @@all = []
-
-  def initialize(name)
+   attr_accessor :name
+   @@all = []
+   def initialize(name)
     @name = name
     @@all << self
   end
-
-  def self.clear_all
-    @@all.clear
+   def self.clear_all
+    @@all = []
   end
-
-  def self.all
-    dog = name.split("\n")
-    @@all.each do |dog|
-      puts dog.name
-    end
+   def self.all
+    @@all.each{|dog|
+    puts dog.name
+    }
   end
-
-end ## end of the class
+ end
